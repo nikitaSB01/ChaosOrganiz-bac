b3721ec889aeb7368e075795c0f25d034ce4e1d6
nikitaSB01 <boss.shvabauer@bk.ru>
2025-03-21 22:10:19 +0500

    доработка фишек

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const http = require("http");
+const WebSocket = require("ws");
 const Koa = require("koa");
 const Router = require("@koa/router");
 const koaBody = require("koa-body").default;
@@ -12,6 +14,26 @@ app.use(koaBody({ multipart: true }));
 
 // Хранилище сообщений в памяти
 const messages = [];
+
+// WebSocket-сервер
+const server = http.createServer(app.callback());
+const wss = new WebSocket.Server({ server });
+const clients = new Set();
+
+wss.on("connection", (ws) => {
+  clients.add(ws);
+  ws.on("close", () => clients.delete(ws));
+});
+
+// Функция для рассылки
+function broadcastMessage(message) {
+  const json = JSON.stringify(message);
+  clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(json);
+    }
+  });
+}
 
 // Получение всех сообщений
 router.get("/messages", (ctx) => {
@@ -36,6 +58,7 @@ router.post("/messages", (ctx) => {
   };
 
   messages.push(message);
+  broadcastMessage(message);
   ctx.body = message;
 });
 
@@ -43,6 +66,6 @@ app.use(router.routes()).use(router.allowedMethods());
 
 // Запуск сервера
 const PORT = process.env.PORT || 7070;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`HTTP + WS server running on http://localhost:${PORT}`);
 });
